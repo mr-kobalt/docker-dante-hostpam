c218ac36d1778000315eea2465dfec4382e37b9d
mr-kobalt <mr.kobalt@gmail.com>
2018-05-24 11:52:25 +0300

    Added some environmental variables options

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
 
 set -e
+
 if [ "$1" == "sockd" ]; then
-  IFACE=$(ip route | grep default | awk '{print $5}')
-  sed -i.bak "s/eth0/$IFACE/" "/etc/sockd.conf"
+  if [ -z "$DEBUG"]; then
+    export DEBUG=0
+  fi
+  sed -i "s/{DEBUG}/$DEBUG/" "/etc/sockd.conf"
+
+  if [ -z "$EXTERNAL_IFACE" ]; then
+    export EXTERNAL_IFACE=$(ip route | grep default | awk '{print $5}')
+  fi
+  sed -i "s/{EXTERNAL_IFACE}/$EXTERNAL_IFACE/" "/etc/sockd.conf"
+
+  if [ -z "$INTERNAL_IFACE" ]; then
+    export INTERNAL_IFACE="0.0.0.0"
+  fi
+  sed -i "s/{INTERNAL_IFACE}/$INTERNAL_IFACE/" "/etc/sockd.conf"
+
+  if [ -z "$PORT"]; then
+    export PORT=1080
+  fi
+  sed -i "s/{PORT}/$PORT/" "/etc/sockd.conf"
+
+  if [ -z "$USER"]; then
+    export USER=sockd
+  fi
+  sed -i "s/{USER}/$PORT/" "/etc/sockd.conf"
 fi
 
 exec "$@"
